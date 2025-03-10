8f72b6f9a25babecd86bf7be21408664f901255c
SakuraBlossomTree <yohib.hussain@gmail.com>
2025-03-10 18:44:36 +0530

    Added torrent download

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::io::{self, BufReader};
 use std::fs::File;
 use scraper::{Html, Selector};
+use std::process::Command;
 
 fn main(){
     let base_url = "https://nyaa.si";
     let search_url = "?q=";
+    let seeders = "&s=seeders";
     let download_query = "/download/";
     let mut links: Vec<String> = Vec::new();
     clearscreen::clear().expect("failed to clear screen");
@@ -17,7 +19,7 @@ fn main(){
     io::stdin().read_line(&mut input).expect("failed to readline");
     println!("Your input {}", input);
     let cleaned_input = input.trim().to_lowercase().replace(" ", "+");
-    let url = format!("{}{}{}", base_url, search_url, cleaned_input);
+    let url = format!("{}{}{}{}", base_url, search_url, cleaned_input, seeders);
     println!("{}", url);
     
     let body = ureq::get(&url)
@@ -29,9 +31,16 @@ fn main(){
     let document = Html::parse_document(&body);
 
     let selector = Selector::parse("a").unwrap();
+    let mut i = 0;
 
     for title in document.select(&selector){
-        println!("{}", title.value().attr("title").into_iter().collect::<String>());
+        if let Some(title) = title.value().attr("title"){
+            let word_count = title.split_whitespace().count();
+            if word_count > 3{
+                i+=1;
+                println!("{} {}", i,title);
+            }
+        }
     }
 
 
@@ -41,22 +50,22 @@ fn main(){
             let word_count = title_cleaned.split_whitespace().count();
             if word_count > 3{
                 if let Some(link) = element.value().attr("href"){
-                    // println!("{}", link);
                     let clean_link = link.replace("/view/", ""); 
                     links.push(clean_link);
                 }
             }
         }
     }
+
     println!("{:?}", links);
+    println!("{}", links.len());
    
     io::stdin().read_line(&mut choice).expect("Failed to read input");
 
     let number: usize = choice.trim().parse().expect("Invalid number");
-    // println!("You entered: {}", number);
 
     if number <= links.len(){
-        let selected_link = &links[number];
+        let selected_link = &links[number - 1];
         let selected_link = format!("{}{}", selected_link, ".torrent");
         println!("{}" , selected_link);
         let torrent = format!("{}{}{}", base_url, download_query,selected_link);
@@ -74,4 +83,8 @@ fn main(){
         println!("Please enter the right choice");
     }
 
+    Command::new("qbittorrent")
+        .arg("downloaded.torrent")
+        .output()
+        .expect("Failed to execute process");
 }
